c7e6669b9c2d33d0f7013f6742b9d95b19038d00
Oleg  Shuliak <nformals@gmail.com>
2023-03-26 21:19:32 +0300

    add HomeWork tasks

---
+++ Sem1C#/Task02HW/Program.cs
@@ -0,0 +1,43 @@
+﻿// Просим пользователя ввести данные для сравнения (тип данных строка)
+Console.WriteLine("Введите первое число");
+string? numLine1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string? numLine2 = Console.ReadLine();
+
+// Проверяем, что данные не пустые
+if ((numLine1 != null) && (numLine2 != null))
+{
+    // Преобразуем данные в числовые
+    int num1 = int.Parse(numLine1);
+    int num2 = int.Parse(numLine2);
+    // Добавляем две переменные для вывода результата (тип данных строка)
+    string max = string.Empty;
+    string min = string.Empty;
+
+    // Сравниваем данные
+    if (num1 > num2)
+    {
+        max = max + num1;
+        min = min + num2;
+    }
+
+    else
+    {
+        max = max + num2;
+        min = min + num1;
+    }
+
+    // Вывод данных в терминал
+    Console.Write("Max = ");
+    Console.WriteLine(max);
+    Console.Write("Min = ");
+    Console.WriteLine(min);
+}
+
+
+
+
+
+
+
+
---
+++ Sem1C#/Task04HW/Program.cs
@@ -0,0 +1,25 @@
+﻿// Просим пользователя ввести три любых числа
+Console.WriteLine("Введите три числа");
+string? numLine1 = Console.ReadLine();
+string? numLine2 = Console.ReadLine();
+string? numLine3 = Console.ReadLine();
+
+// Проверяем, чтобы данные не были пустыми
+if ((numLine1 != null) && (numLine2 != null) && (numLine3 != null))
+{
+    // Парсим данные
+    int num1 = int.Parse(numLine1);
+    int num2 = int.Parse(numLine2);
+    int num3 = int.Parse(numLine3);
+
+    // Вводим переменную для вывода результата равную первому введенному значению
+    int max = num1;
+
+    // Сравниваем оставшиеся введеннные пользователем значения
+    if (num2 > max) max = num2;
+    if (num3 > max) max = num3;
+
+    // Выводим результат в терминале
+    Console.Write("Max = ");
+    Console.WriteLine(max);
+}
