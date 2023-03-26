6536ee2a0a8cb4ea1dd95c142c11c2c20d64f982
Oleg  Shuliak <nformals@gmail.com>
2023-03-26 11:40:39 +0300

    add Task00

---
+++ Sem1C#/Task00/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считываем данные с консоли
+string? inLine = Console.ReadLine();
+
+// Проверим, чтобы данные были не пустыми
+if (inLine != null)
+
+{
+    // Преобразовываем строковое значение в целочисленное (парсим)
+    int inNumber = int.Parse(inLine);
+
+    // Находим квадрат числа
+    // int outNumb = inNumber * inNumber;
+    int outStrtPow = (int)Math.Pow(inNumber,2);
+
+    // Выводим данные в консоль
+    // Console.WriteLine(outNumb);
+    Console.WriteLine(outStrtPow);
+
+}
